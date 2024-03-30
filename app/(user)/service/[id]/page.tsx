125170567cefc752f59b832a7334ca472b1b6898
longpiseth2002 <stevkhat121212@gmail.com>
2024-03-31 04:22:02 +0700

    Update Service page

--- app/(user)/service/[id]/page.tsx
+++ app/(user)/service/[id]/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Card } from 'flowbite-react'
-import CardDetailComponent from '@/component/CardDetailComponent'
+import React from 'react';
+import { Card } from 'flowbite-react';
+import CardDetailComponent from '@/component/CardDetailComponent';
 
 type PropsType = {
     params: {
@@ -17,16 +17,34 @@ export const getData = async (id: number) => {
     return data;
 }
 
-export default async function page(props: PropsType) {
-    const data = await getData(props.params.id);
-    return (
+export default function Page(props: PropsType) {
+    const { id } = props.params;
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await getData(id);
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
         
-        <div className='h-screen grid place-content-center justify-center '>
+        fetchData();
+    }, [id]);
+
+    if (!data) {
+        return <div>Loading...</div>; // Or use a loading component
+    }
+
+    return (
+        <div className='h-screen grid place-content-center justify-center'>
             <CardDetailComponent
-            title={data.title}
-            description={data.description}
-            image={data.image}
-        />
+                title={data.title}
+                description={data.description}
+                image={data.image}
+            />
         </div>
     );
 }
