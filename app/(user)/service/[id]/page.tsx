9b1dc5ceb57d6afd625b14739fd01f3c4ea10959
longpiseth2002 <stevkhat121212@gmail.com>
2024-03-31 04:38:24 +0700

    Update Service page

--- app/(user)/service/[id]/page.tsx
+++ app/(user)/service/[id]/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Card } from 'flowbite-react'
-import CardDetailComponent from '@/component/CardDetailComponent'
+import React, { useEffect, useState } from 'react';
+import { Card } from 'flowbite-react';
+import CardDetailComponent from '@/component/CardDetailComponent';
 
 type PropsType = {
     params: {
@@ -17,16 +17,30 @@ export const getData = async (id: number) => {
     return data;
 }
 
-export default async function page(props: PropsType) {
-    const data = await getData(props.params.id);
+const Page: React.FC<PropsType> = (props) => {
+    const [data, setData] = useState<any>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getData(props.params.id);
+            setData(result);
+        };
+        fetchData();
+    }, [props.params.id]);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        
         <div className='h-screen grid place-content-center justify-center '>
             <CardDetailComponent
-            title={data.title}
-            description={data.description}
-            image={data.image}
-        />
+                title={data.title}
+                description={data.description}
+                image={data.image}
+            />
         </div>
     );
 }
+
+export default Page;
