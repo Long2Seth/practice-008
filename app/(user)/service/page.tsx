8acad4cb2b3455d5eed905e09d8b190907ca499b
longpiseth2002 <stevkhat121212@gmail.com>
2024-03-31 04:18:10 +0700

    Update Service page

--- app/(user)/service/page.tsx
+++ app/(user)/service/page.tsx
@@ -1,25 +1,17 @@
 'use client';
-import ProductCardComponent from '@/component/ProductCardComponent'
+import ProductCardComponent from '@/component/ProductCardComponent';
 import { useRouter } from 'next/navigation';
-import React, { use } from 'react'
-import { useState,useEffect } from 'react'
-import Loading from './component/loading'
-
+import React, { useState, useEffect } from 'react';
+import Loading from './component/loading';
 
 const ENDPOINT = "https://fakestoreapi.com/products/";
 
-export default function page() {
-
-
+export default function Page() {
   const [getData, setData] = useState([]);
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
 
-  
-  
-
   useEffect(() => {
-
     fetch(ENDPOINT)
       .then((res) => res.json())
       .then((data) => {
@@ -32,26 +24,21 @@ export default function page() {
       });
   }, []);
 
-
   if (isLoading) {
     return <Loading />;
   }
 
-
   return (
-    <div className=' grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center gap-5 m-16'>{
-      getData.map((item: any , index) => (
+    <div className=' grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center gap-5 m-16'>
+      {getData.map((item:any, index) => (
         <ProductCardComponent
-        onClick={() => router.push(`/service/${item.id}`)}
-        key={index} 
-        title={item.title}
-        price={item.price}
-        image={item.image}
+          onClick={() => router.push(`/service/${item.id}`)}
+          key={index} 
+          title={item.title}
+          price={item.price}
+          image={item.image}
         />
-      ))
-    }
-      
+      ))}
     </div>
-
-  )
+  );
 }
